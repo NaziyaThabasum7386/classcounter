ecad14d70e1e1254e8352b92b0deb5cecb3e677f
nimagalunaziya <nimagalunaziya@gmail.com>
2024-03-22 21:43:03 +0530

    Test commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: this.props.start,
+    };
+  }
+
+  handleClick = (e) => {
+    e.preventdefault()
+    this.setState((prevState) => {
+      return {
+        count: prevState.count - (this.props.jump || 1),
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <h3>Count : {this.state.count}</h3>
+        <button onClick={this.handleClick}>Decrement</button>
+        <button
+          onClick={(e) => {
+            this.setState((prevState) => {
+              return {
+                count: prevState.count + (this.props.jump || 1),
+              };
+            });
+          }}
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <Counter start={1} />
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
